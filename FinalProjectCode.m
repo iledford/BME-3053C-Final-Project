e9aa5224addc2c34c537eb1a24045335e3eccd82
jprieschl <94015248+jprieschl@users.noreply.github.com>
2021-12-01 19:11:08 -0600

    Update FinalProjectCode.m

--- FinalProjectCode.m
+++ FinalProjectCode.m
@@ -23,9 +23,14 @@ clc; clear;
 %% Establish Folder Pathway for Subject
 
 pathway = input('What is the pathway to the dicom image folder? ','s');
+fprintf('Pathway has been established...\n')
+pause(1)
+
+fprintf('Enter a term (Subject1, Subject2, etc.) that you want associated with output files. Do NOT leave a space between the word subject and the corresponding number...\n')
 OutputFileName = input('What subject does the CT scan correspond to (for our study)? ','s'); %replace this with the subject number you want to see in the output files
 
 addpath(pathway)
+fprintf('\nPathway has been added!\n')
 
 %% Create pathways and variables for length and prevalence matrix
 folderpathway = dir(pathway); %put in folder name
@@ -33,11 +38,17 @@ folderpathway = folderpathway(3:end);
 lengthFolder = length(folderpathway);
 PrevalenceMatrix = [];
 
+fprintf('Length of the input folder has been determined...\n')
+pause(0.5)
+
 %% Scan each of the .dcm images in the folder to determine prevalence of white pixels
+fprintf('Scanning each .dcm/file in the input folder...\n')
 prevalence = 0;
 for i = 1:1:lengthFolder
     file = folderpathway(i).name;
     if file(1) == 'S'
+        fprintf("You already have a subject's output files in a folder. Check to make sure which one is correct!\n")
+        clc; pause(3)
         break
     end 
     info = file;
@@ -55,6 +66,9 @@ for i = 1:1:lengthFolder
     PrevalenceMatrix(i,:) = PrevalencePercent;
 end 
 
+fprintf('The Prevalence Matrix has been compiled.\n')
+pause(0.5)
+
 %% Find Statistical Differences in Data (from PrevalenceMatrix)
 
 meanPrevalence = mean(PrevalenceMatrix); %mean of the prevalence
@@ -62,6 +76,8 @@ Sdev = std(PrevalenceMatrix); %stan. deviation of the prevalence
 
 %add more measures of the data here 
 varPrevalence = var(PrevalenceMatrix); %variance of the prevalence 
+
+fprintf('The mean, standard deviation, and variance has been computed for the entire dataset for this subject.\n')
 
 %% Set up output variables and prepare the table
 txtOutputFilename = append(OutputFileName,'.txt');
@@ -72,6 +88,10 @@ Titles = {'Mean';'Standard Deviation';'Variance'};
 C = {'Mean', meanPrevalence; 'Standard Dev.', Sdev; 'Variance', varPrevalence};
 T = table(Titles, results);
 
+fprintf('The table and cell array are complete. ')
+pause(0.5)
+fprintf('Preparing to export...\n')
+
 %% Sending Results to a Folder (SubjectResults_COVID) in Files
 
 % Display a new current folder in flash drive
@@ -79,4 +99,9 @@ T = table(Titles, results);
 % Create output files in current folder
 writecell(C, datOutputFilename)
 writetable(T, txtOutputFilename)
+
+fprintf('The .dat and .txt files have been exported!\n')
+pause(0.5)
+fprintf('CT scan analysis is complete.')
+
 
